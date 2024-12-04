12702ed48a60cae928f552282c2f6536626f4d09
Alejandro Notario <alealenotario@gmail.com>
2024-12-04 01:32:07 -0300

    Rutas con Route::resource

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,21 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 Route::view('/contactos', 'contact')->name('contact');
 
-Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
-Route::get('/blog/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/blog', [PostController::class, 'store'])->name('posts.store');
-Route::get('/blog/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::patch('/blog/{post}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/blog/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+//Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
+//Route::get('/blog/create', [PostController::class, 'create'])->name('posts.create');
+//Route::post('/blog', [PostController::class, 'store'])->name('posts.store');
+//Route::get('/blog/{post}', [PostController::class, 'show'])->name('posts.show');
+//Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+//Route::patch('/blog/{post}', [PostController::class, 'update'])->name('posts.update');
+//Route::delete('/blog/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+Route::resource('blog', PostController::class, [
+    'names' => 'posts',
+    'parameters' => [
+        'blog' => 'post'
+    ]
+
+]);
 
 Route::view('/nosotros', 'about')->name('about');
 
