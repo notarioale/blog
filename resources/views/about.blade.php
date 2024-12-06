1e6c82f645ede544174ed0dfbc6557a968ea2068
Alejandro Notario <alealenotario@gmail.com>
2024-12-06 17:22:02 -0300

    Mas CSS

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,7 @@
 <x-layout meta-tittle="About tittle" meta-description="About description">
-    <h1>About</h1>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            About
+        </h1>
+    </div>
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
 
 <x-layout meta-tittle="Home tittle" meta-description="Home description">
-    <h1>Inicio</h1>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Inicio
+        </h1>
+    </div>
 </x-layout>
